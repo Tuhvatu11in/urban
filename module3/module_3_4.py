b0727e5962fb46b8158e4b6781a73b5bf3414168
Tuhvatu11in <135365671+Tuhvatu11in@users.noreply.github.com>
2024-09-10 14:13:46 +0300

    3

--- module3/module_3_4.py
+++ module3/module_3_4.py
@@ -1,9 +1,13 @@
 def single_root_words(root_word, *other_words):
+
   same_words = []
+  root_word = root_word.lower()  # Приводим root_word к нижнему регистру
   for word in other_words:
+    word = word.lower()  # Приводим word к нижнему регистру
     if root_word in word or word in root_word:
       same_words.append(word)
   return same_words
 
+# Вызов функции и вывод результата
 result = single_root_words('дом', 'домик', 'дерево', 'домовладение', 'домашний')
 print(result)  # Вывод: ['домик', 'домовладение', 'домашний']

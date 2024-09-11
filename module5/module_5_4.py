4c6a5bb2a8058d3d6fb3690858103b5edcc6f93f
Tuhvatu11in <135365671+Tuhvatu11in@users.noreply.github.com>
2024-09-11 09:49:10 +0300

    asdf

---
+++ module5/module_5_4.py
@@ -0,0 +1,88 @@
+class House:
+     houses_history = []
+
+    def __new__(cls, name, floors):
+        instance = super().__new__(cls)
+        instance.name = name
+        instance.floors = floors
+        cls.houses_history.append(name)
+        return instance
+
+    
+    
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_floors:
+            for i in range(1, new_floor + 1):
+                print(i)
+        else:
+            print("Такого этажа не существует")
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
+        return False
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors <= other
+        return False
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors > other
+        return False
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors >= other
+        return False
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors != other
+        return True
+
+    def __add__(self, other):
+        if isinstance(other, int):
+            self.number_of_floors += other
+            return self
+        return NotImplemented
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __iadd__(self, other):
+        if isinstance(other, int):
+            self.number_of_floors += other
+            return self
+        return NotImplemented
+
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")

5ca50279976f0203d6268b8033e41ec2922e460d
Tuhvatu11in <135365671+Tuhvatu11in@users.noreply.github.com>
2024-09-14 16:58:56 +0300

    85

--- module12/test_12_3/tests_12_3.py
+++ module12/test_12_3/tests_12_3.py
@@ -1,53 +1,73 @@
+# suite_12_3.py
 import unittest
 
-#  TestCase RunnerTest
 class RunnerTest(unittest.TestCase):
     is_frozen = False
 
-    @classmethod
-    def setUpClass(cls):
-        print("Setup for RunnerTest")
+    def setUp(self):
+        self.runner = Runner("Bob")
 
-    @classmethod
-    def tearDownClass(cls):
-        print("Teardown for RunnerTest")
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_init(self):
+        self.assertEqual(self.runner.name, "Bob")
+        self.assertEqual(self.runner.distance, 0)
+        self.assertEqual(self.runner.speed, 5)
 
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
     def test_run(self):
-        if self.is_frozen:
-            self.skipTest("Тесты в этом кейсе заморожены")
-        print("Проверка метода run")
+        self.runner.run()
+        self.assertEqual(self.runner.distance, 10)
 
-    def test_stop(self):
-        if self.is_frozen:
-            self.skipTest("Тесты в этом кейсе заморожены")
-        print("Проверка метода stop")
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_walk(self):
+        self.runner.walk()
+        self.assertEqual(self.runner.distance, 5)
 
-#  TestCase TournamentTest
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_str(self):
+        self.assertEqual(str(self.runner), "Bob")
+
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_eq_with_string(self):
+        self.assertTrue(self.runner == "Bob")
+
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_eq_with_runner(self):
+        other_runner = Runner("Bob")
+        self.assertTrue(self.runner == other_runner)
+
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_eq_with_different_runner(self):
+        other_runner = Runner("Alice")
+        self.assertFalse(self.runner == other_runner)
+
+
 class TournamentTest(unittest.TestCase):
     is_frozen = True
 
-    @classmethod
-    def setUpClass(cls):
-        print("Setup for TournamentTest")
+    def setUp(self):
+        self.runner1 = Runner("Bob")
+        self.runner2 = Runner("Alice", speed=7)
+        self.tournament = Tournament(20, self.runner1, self.runner2)
 
-    @classmethod
-    def tearDownClass(cls):
-        print("Teardown for TournamentTest")
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_init(self):
+        self.assertEqual(self.tournament.full_distance, 20)
+        self.assertEqual(self.tournament.participants, [self.runner1, self.runner2])
 
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
     def test_start(self):
-        if self.is_frozen:
-            self.skipTest("Тесты в этом кейсе заморожены")
-        print("Проверка метода start")
+        finishers = self.tournament.start()
+        self.assertEqual(len(finishers), 2)
+        self.assertEqual(finishers[1].name, "Alice")
+        self.assertEqual(finishers[2].name, "Bob")
 
-    def test_finish(self):
-        if self.is_frozen:
-            self.skipTest("Тесты в этом кейсе заморожены")
-        print("Проверка метода finish")
 
-#  TestSuite
-test_suite = unittest.TestSuite()
-test_suite.addTest(unittest.makeSuite(RunnerTest))
-test_suite.addTest(unittest.makeSuite(TournamentTest))
+suite = unittest.TestSuite()
+
+suite.addTest(unittest.makeSuite(RunnerTest))
+suite.addTest(unittest.makeSuite(TournamentTest))
 
 runner = unittest.TextTestRunner(verbosity=2)
-result = runner.run(test_suite)
+
+result = runner.run(suite)

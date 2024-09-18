1c36aaa5aeaf0f23d7598a8b9cd0f42b85d9e68a
Tuhvatu11in <135365671+Tuhvatu11in@users.noreply.github.com>
2024-09-18 17:16:37 +0300

    77

--- module13/module_13_3.py
+++ module13/module_13_3.py
@@ -13,12 +13,10 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     await message.answer("Привет! Я бот помогающий твоему здоровью.")
-    print("Пользователь начал общение.")
 
 @dp.message_handler()
 async def all_messages(message: types.Message):
     await message.answer("Введите команду /start, чтобы начать общение.")
-    print("Пользователь ввёл:", message.text)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)

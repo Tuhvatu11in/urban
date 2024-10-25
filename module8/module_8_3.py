3c31e463ca4986f93ba2d5933b7d81f94f9e6135
Tuhvatu11in <135365671+Tuhvatu11in@users.noreply.github.com>
2024-10-25 19:28:52 +0300

    Update module_8_3.py

--- module8/module_8_3.py
+++ module8/module_8_3.py
@@ -1,36 +1,36 @@
 class IncorrectVinNumber(Exception):
     def __init__(self, message):
+        super().__init__(message)
         self.message = message
 
 class IncorrectCarNumbers(Exception):
     def __init__(self, message):
+        super().__init__(message)
         self.message = message
 
 class Car:
-    def __init__(self, name, vin, numbers):
-        self.name = name
 
-        # Проверка VIN перед присвоением
-        if not self.__is_valid_vin(vin):
-            raise IncorrectVinNumber('Некорректный vin номер')
+    def __init__(self, model, vin, numbers):
+        self.model = model
         self.__vin = vin
-
-        # Проверка номеров перед присвоением
-        if not self.__is_valid_numbers(numbers):
-            raise IncorrectCarNumbers('Некорректные номера')
         self.__numbers = numbers
 
+        self.__is_valid_vin(self.__vin)
+
+        self.__is_valid_numbers(self.__numbers)
+
     def __is_valid_vin(self, vin_number):
+
         if not isinstance(vin_number, int):
-            return False
+            raise IncorrectVinNumber('Некорректный тип vin номер')
         if not 1000000 <= vin_number <= 9999999:
-            return False
+            raise IncorrectVinNumber('Неверный диапазон для vin номера')
         return True
 
     def __is_valid_numbers(self, numbers):
-        if not isinstance(numbers, str):
-            return False
-        if len(numbers) != 6:
-            return False
-        return True
 
+        if not isinstance(numbers, str):
+            raise IncorrectCarNumbers('Некорректный тип данных для номеров')
+        if len(numbers) != 6:
+            raise IncorrectCarNumbers('Неверная длина номера')
+        return True

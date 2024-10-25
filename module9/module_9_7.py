43b0c91c5f546564d6284dfaf8f3e3f65fa547e9
Tuhvatu11in <135365671+Tuhvatu11in@users.noreply.github.com>
2024-10-25 13:39:52 +0300

    1234567o

--- module9/module_9_7.py
+++ module9/module_9_7.py
@@ -4,6 +4,9 @@ def sum_three(a, b, c):
 def is_prime(func):
   def wrapper(*args, **kwargs):
     result = func(*args, **kwargs)
+    if result == 1:
+      print("Ошибка, еденица является не простым, не составным!")
+      return result
     if result > 1:
       for i in range(2, int(result**0.5) + 1):
         if result % i == 0:

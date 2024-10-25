966358310b8b73aa696b52d7234f112fbf37174f
Tuhvatu11in <135365671+Tuhvatu11in@users.noreply.github.com>
2024-10-25 13:00:38 +0300

    2

--- module8/module_8_3.py
+++ module8/module_8_3.py
@@ -9,21 +9,28 @@ class IncorrectCarNumbers(Exception):
 class Car:
     def __init__(self, name, vin, numbers):
         self.name = name
+
+        # Проверка VIN перед присвоением
+        if not self.__is_valid_vin(vin):
+            raise IncorrectVinNumber('Некорректный vin номер')
         self.__vin = vin
+
+        # Проверка номеров перед присвоением
+        if not self.__is_valid_numbers(numbers):
+            raise IncorrectCarNumbers('Некорректные номера')
         self.__numbers = numbers
-        self.__is_valid_vin(self.__vin)
-        self.__is_valid_numbers(self.__numbers)
 
     def __is_valid_vin(self, vin_number):
         if not isinstance(vin_number, int):
-            raise IncorrectVinNumber('Некорректный тип vin номер')
+            return False
         if not 1000000 <= vin_number <= 9999999:
-            raise IncorrectVinNumber('Неверный диапазон для vin номера')
+            return False
         return True
 
     def __is_valid_numbers(self, numbers):
         if not isinstance(numbers, str):
-            raise IncorrectCarNumbers('Некорректный тип данных для номеров')
+            return False
         if len(numbers) != 6:
-            raise IncorrectCarNumbers('Неверная длина номера')
+            return False
         return True
+
